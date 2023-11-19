d68329581640e6cf476de5a342a255d57dfcddc4
Georg Kreuzmayr <g.kreuzmayr@gmail.com>
2023-11-19 05:39:54 +0100

    delete duplicates

--- backend/queries/__init__.py
+++
@@ -1,5 +0,0 @@
-# __init__.py
-from .find_craftsmen_by_postcode import find_craftsmen_by_postcode_query 
-from .ingest_data import ingest_data_query 
-from .schema import schema_query 
-from .update_craftsmen import update_craftsmen_query 
--- backend/queries/ingest_data.py
+++
@@ -1,5 +0,0 @@
-ingest_data_query ='''
-INSERT INTO postcode SELECT * FROM read_parquet('data/postcode.parquet');
-INSERT INTO quality_factor_score SELECT * FROM read_parquet('data/quality_factor_score.parquet');
-INSERT INTO service_provider_profile SELECT * FROM read_parquet('data/service_provider_profile.parquet');
-'''
