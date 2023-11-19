aaf59119a6dcdcde9ca769b262b96afd362b4f32
Georg Kreuzmayr <g.kreuzmayr@gmail.com>
2023-11-19 05:49:44 +0100

    fix port

--- frontend/src/data/query-coordination/asyncFetcher.tsx
+++ frontend/src/data/query-coordination/asyncFetcher.tsx
@@ -2,7 +2,7 @@ import * as duckdb from '@duckdb/duckdb-wasm';
 import {CraftsmanDto} from "../../types/craftsmanTypes.ts";
 
 export async function fetchCraftsmenAsync(postcode: string, conn?: duckdb.AsyncDuckDBConnection) {
-    return fetch("http://18.194.1.200:3000/craftsmen?postalcode=" + postcode).then(res => res.json()).then((rows) => {
+    return fetch("http://18.194.1.200:8000/craftsmen?postalcode=" + postcode).then(res => res.json()).then((rows) => {
         if (rows.length != 0) {
             let qs = "INSERT OR REPLACE INTO service_provider_profile(id, first_name, last_name, lat, lon, profile_score) VALUES";
             rows.forEach((row: CraftsmanDto) => {
