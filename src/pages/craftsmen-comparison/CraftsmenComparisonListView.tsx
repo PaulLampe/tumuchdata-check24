8cf67e9339f806afe2924d71ad21ce8d84ca2280
paullampe <paul@mwlampe.de>
2023-11-19 05:05:00 +0100

    Add load more infra

--- src/pages/craftsmen-comparison/CraftsmenComparisonListView.tsx
+++ src/pages/craftsmen-comparison/CraftsmenComparisonListView.tsx
@@ -5,12 +5,33 @@ import {
 } from "@chakra-ui/react";
 import CraftsmanCard from "./CrafstmanCard.tsx";
 import {CraftsmanDto} from "../../types/craftsmanTypes.ts";
+import {useRef} from "react";
 
 
 export default function CraftsmenComparisonListView({craftsmen}: { craftsmen: CraftsmanDto[] }) {
+
+    const lastCount = useRef(0);
+
+    function loadMore() {
+        if (lastCount.current != craftsmen.length) {
+            lastCount.current = craftsmen.length;
+            console.log("loadMore");
+        }
+    }
+
     return (
         <Center flex={1} width={"100%"} height={"100%"}>
-            <List overflow={"scroll"} width={"100%"} height={"100%"} maxWidth={"container.lg"}>
+            <List
+                overflow={"scroll"}
+                width={"100%"}
+                height={"100%"}
+                maxWidth={"container.lg"}
+                onScroll={(e) => {
+                    if ((e.currentTarget.scrollTop + e.currentTarget.clientHeight) / e.currentTarget.scrollHeight > 0.8) {
+                        loadMore();
+                    }
+                }}
+            >
                 {
                     craftsmen?.map((c) => (
                         <ListItem key={c.id}>
